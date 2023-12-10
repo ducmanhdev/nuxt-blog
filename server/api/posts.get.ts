ee00a1f2476e3c40a5ff9013d9bb45beacf97344
work.ducmanh0323 <work.ducmanh0323@gmail.com>
2023-12-10 08:40:50 +0700

    Refactor server API and pagination features

--- server/api/posts.get.ts
+++ server/api/posts.get.ts
@@ -1,7 +1,16 @@
 import { Post } from '~/server/models/Post';
 import APIFeatures from '~/utils/api-features';
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
   const query = getQuery(event);
-  return new APIFeatures(Post.find(), query).filter().sort().limitFields().paginate();
+  const features = new APIFeatures(Post.find(), query).filter().sort().limitFields().paginate();
+
+  const data = await features.query;
+
+  const metadata = await features.meta();
+
+  return {
+    data,
+    metadata,
+  };
 });
--- utils/api-features.ts
+++ utils/api-features.ts
@@ -15,9 +15,7 @@ export default class APIFeatures {
     // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
     this.query = this.query.find(JSON.parse(queryStr));
-
     return this;
   }
 
@@ -28,7 +26,6 @@ export default class APIFeatures {
     } else {
       this.query = this.query.sort('-createdAt');
     }
-
     return this;
   }
 
@@ -39,40 +36,26 @@ export default class APIFeatures {
     } else {
       this.query = this.query.select('-__v');
     }
-
     return this;
   }
 
-  async paginate() {
+  paginate() {
     const page = this.queryString.page * 1 || 1;
     const limit = this.queryString.limit * 1 || 10;
     const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
 
-    let result = await this.query.model.aggregate([
-      {
-        $facet: {
-          data: [{ $skip: skip }, { $limit: limit }],
-          metaData: [
-            {
-              $count: 'totalItems',
-            },
-            {
-              $addFields: {
-                currentPage: page,
-                itemsPerPage: limit,
-                totalPages: {
-                  $ceil: {
-                    $divide: ['$totalItems', limit],
-                  },
-                },
-              },
-            },
-          ],
-        },
-      },
-    ]);
-    result = result[0];
-    result.metaData = result.metaData[0];
-    return result;
+  async meta() {
+    const total = await this.query.model.estimatedDocumentCount();
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 10;
+    return {
+      totalItems: total,
+      currentPage: page,
+      itemsPerPage: limit,
+      totalPages: Math.ceil(total / limit),
+    };
   }
 }
