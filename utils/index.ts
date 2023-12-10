dc363629f578fca8e123279ed88eb31d11c74770
work.ducmanh0323 <work.ducmanh0323@gmail.com>
2023-12-10 08:43:14 +0700

    Refactor 'slugify' function for improved readability

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,15 @@
 export const slugify = (str: string) => {
+  // Regular expressions for slugify function.
+  const ACCENTED_CHARS = /[\u0300-\u036F]/g;
+  const NON_ALPHANUMERIC = /[^a-z0-9 -]/g;
+  const SPACES = /\s+/g;
+  const CONSECUTIVE_HYPHENS = /-+/g;
   return String(str)
     .normalize('NFKD') // split accented characters into their base characters and diacritical marks
-    .replace(/[\u0300-\u036F]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+    .replace(ACCENTED_CHARS, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
     .trim() // trim leading or trailing whitespace
     .toLowerCase() // convert to lowercase
-    .replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
-    .replace(/\s+/g, '-') // replace spaces with hyphens
-    .replace(/-+/g, '-'); // remove consecutive hyphens
+    .replace(NON_ALPHANUMERIC, '') // remove non-alphanumeric characters
+    .replace(SPACES, '-') // replace spaces with hyphens
+    .replace(CONSECUTIVE_HYPHENS, '-'); // remove consecutive hyphens
 };
