957f9919402075145d830a93bccda5d111c51adb
work.ducmanh0323 <work.ducmanh0323@gmail.com>
2023-12-21 21:18:20 +0700

    Modify validateUser function to handle optional throwError

--- server/helpers.ts
+++ server/helpers.ts
@@ -2,16 +2,17 @@ import { EventHandlerRequest, H3Event } from 'h3';
 import { getServerSession } from '#auth';
 import User from '~/server/models/User';
 
-export const validateUser = async (event: H3Event<EventHandlerRequest>) => {
+export const validateUser = async (event: H3Event<EventHandlerRequest>, throwError = true) => {
   const session = await getServerSession(event);
   const userId = (session?.user as any)?._id;
 
   if (!userId) {
+    if (!throwError) return null;
     throw createError({ statusMessage: 'Unauthenticated', statusCode: 403 });
   }
-
-  const user = await User.findById(userId);
+  const user = await User.findById(userId).lean();
   if (!user) {
+    if (!throwError) return null;
     throw createError({ statusMessage: 'Unauthenticated', statusCode: 403 });
   }
 
