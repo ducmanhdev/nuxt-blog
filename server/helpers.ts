98c6bcdc76f9da2a94443dd382b8e28143e2471e
work.ducmanh0323 <work.ducmanh0323@gmail.com>
2024-01-01 14:38:24 +0700

    Refactor user validation in server helpers

--- server/helpers.ts
+++ server/helpers.ts
@@ -4,16 +4,10 @@ import User from '~/server/models/User';
 
 export const validateUser = async (event: H3Event<EventHandlerRequest>) => {
   const session = await getServerSession(event);
-  console.log(session);
   const userId = (session?.user as any)?._id;
-
-  if (!userId) {
-    throw createError({ statusMessage: 'Unauthenticated missingUserId', statusCode: 403 });
-  }
   const user = await User.findById(userId);
-  if (!user) {
+  if (!userId || !user) {
     throw createError({ statusMessage: 'Unauthenticated user', statusCode: 403 });
   }
-
   return user;
 };
